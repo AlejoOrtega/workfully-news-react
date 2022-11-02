d554bc12c4884f154049199ec078090ecd6f77ca
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-11-02 09:56:56 -0400

    Change: Headers a-tags for components buttons

--- src/shared/components/Header/Header.jsx
+++ src/shared/components/Header/Header.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import style from "./Header.module.css";
 import logo from "../../../assets/icons/workfully_black_465x.avif"
-
+import Button from "../Button/Button";
 
 
 const Header = () => {
+  const onReadersView = () => {
+    window.location.href = "/"
+  }
+  const onJournalistView = () => {
+    window.location.href = "/admin"
+  }
   return (
     <section className={`${style.header}`}>
       <div className={style.container}>
         <a href ="/home"><img className={style.wlogo} src={logo} alt= 'workfully'/></a>
         <div className="user-roles">
         {/* <Link to="/" className="btn-primary">Reader</Link> */}
-            <a className={style.role} href="/">Reader</a>
-            <a className={style.role} href="/admin">Journalist</a>
+            <Button children={<h1>Reader</h1>} handleClick={onReadersView} />
+            <Button children={<h1>Journalist</h1>} handleClick={onJournalistView} />
+            {/* <a className={style.role} href="/">Reader</a>
+            <a className={style.role} href="/admin">Journalist</a> */}
         </div>
       </div>
     </section>
