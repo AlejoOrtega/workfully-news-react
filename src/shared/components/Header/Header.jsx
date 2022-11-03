8c8776b706677335f17b78a8456acf5ceca46900
JoaoMaGuimaraes <joaomaguimaraes39@gmail.com>
2022-11-03 10:21:25 +0000

    Add workfully logo based on theme

--- src/shared/components/Header/Header.jsx
+++ src/shared/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "./Header.module.css";
-import logo from "../../../assets/icons/workfully_black_465x.avif";
+import logoLight from "../../../assets/icons/workfully_black_465x.avif";
+import logoDark from "../../../assets/icons/workfully_white.png";
 import iconLight from "../../../assets/icons/icon-light-theme.svg";
 import iconDark from "../../../assets/icons/icon-dark-theme.svg";
 import { ThemeContext } from "../../context/ThemeContext";
@@ -9,26 +10,25 @@ import { ThemeContext } from "../../context/ThemeContext";
 
 
 const Header = () => {
-    const { theme, setTheme } = React.useContext(ThemeContext);
+  const { theme, setTheme } = React.useContext(ThemeContext);
 
-    const handleTheme = () => {
-        setTheme(!theme);
-        console.log(theme)
-      };
+  const handleTheme = () => {
+    setTheme(!theme);
+    console.log(theme)
+  };
 
-      let themeMode = theme ? "light" : "dark";
+  let themeMode = theme ? "light" : "dark";
 
   return (
     <section className={`${style.header}  ${style[`header--${themeMode}`]}`}>
       <div className={style.container}>
-        <a href ="/home"><img className={style.wlogo} src={logo} alt= 'workfully'/></a>
+        <a href="/home"><img className={style.wlogo} src={theme ? logoLight : logoDark} alt='workfully' /></a>
         <div className="user-roles">
-        {/* <Link to="/" className="btn-primary">Reader</Link> */}
-            <a className={style.role} href="/">Reader</a>
-            <a className={style.role} href="/admin">Journalist</a>
-            <button onClick={handleTheme}>
-                <img src={theme ? iconLight : iconDark} />
-            </button>
+          <a className={style.role} href="/">Reader</a>
+          <a className={style.role} href="/admin">Journalist</a>
+          <button onClick={handleTheme}>
+            <img src={theme ? iconLight : iconDark} />
+          </button>
         </div>
       </div>
     </section>
